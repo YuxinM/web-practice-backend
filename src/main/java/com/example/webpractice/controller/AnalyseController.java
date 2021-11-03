82c5a51ea205831cf29256f0f81e5d32e87675ad
PY <181250111@smail.nju.edu.cn>
2021-11-03 23:59:39 +0800

    springdoc

--- src/main/java/com/example/webpractice/controller/AnalyseController.java
+++ src/main/java/com/example/webpractice/controller/AnalyseController.java
@@ -1,8 +1,9 @@
 package com.example.webpractice.controller;
 
 import com.example.webpractice.bl.AnalyseService;
-import com.example.webpractice.bl.PaperService;
 import com.example.webpractice.vo.ResponseVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 
 @CrossOrigin
+@Tag(name = "内规接口", description = "内化法规相关操作")
 @Controller
 @RequestMapping("/api/analyse")
 public class AnalyseController {
@@ -21,12 +23,14 @@ public class AnalyseController {
     @Autowired
     AnalyseService analyseService;
 
+    @Operation(summary = "获取内规详情", description = "根据ID获取内规详情")
     @GetMapping("/get/{id}")
     @ResponseBody
     public ResponseVO getAnalyseById(@PathVariable(value = "id") int id) {
         return analyseService.getAnalyseById(id);
     }
 
+    @Operation(summary = "新增内规", description = "新增内规")
     @PostMapping("/add")
     @ResponseBody
     public ResponseVO addPaper(@RequestParam(value = "title") String title,
@@ -41,7 +45,8 @@ public class AnalyseController {
         return analyseService.addAnalyse(title, number, category, interpret, input_user, input_time, file, paper_id);
     }
 
-    @PostMapping("/update/{id}")
+    @Operation(summary = "更新内规", description = "根据ID更新内规")
+    @PutMapping("/update/{id}")
     @ResponseBody
     public ResponseVO update(@PathVariable("id") int id,
                              @RequestParam(value = "title") String title,
--- src/main/java/com/example/webpractice/controller/UserController.java
+++ src/main/java/com/example/webpractice/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.webpractice.bl.UserService;
 import com.example.webpractice.util.SessionManager;
 import com.example.webpractice.vo.ResponseVO;
 import com.example.webpractice.vo.UserVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
  */
 
 @CrossOrigin
+@Tag(name = "用户接口", description = "用户账号相关操作")
 @Controller
 @RequestMapping("/api/user")
 public class UserController {
@@ -22,18 +25,21 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Operation(summary = "注册", description = "新增用户账号")
     @PostMapping("/register")
     @ResponseBody
     public ResponseVO register(@RequestBody UserVO userVO) {
         return userService.register(userVO);
     }
 
+    @Operation(summary = "登录", description = "用户登录账号")
     @PostMapping("/login")
     @ResponseBody
     public ResponseVO login(@RequestBody UserVO userVO) {
         return userService.login(userVO);
     }
 
+    @Operation(summary = "登出", description = "用户登出账号")
     @PostMapping("/logout/{id}")
     @ResponseBody
     public ResponseVO logout(@PathVariable("id") int id) {
